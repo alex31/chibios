a73b1608b4fda097eab422eddeff9f32d39006e8
Giovanni Di Sirio <gdisirio@gmail.com>
2021-06-13 16:12:20 +0000

    Release ver_21.6.0

--- os/ex/include/ex.h
+++ os/ex/include/ex.h
@@ -36,12 +36,12 @@
 /**
  * @brief   ChibiOS/EX identification macro.
  */
-#define _CHIBIOS_EX_
+#define __CHIBIOS_EX__
 
 /**
  * @brief   Stable release flag.
  */
-#define CH_EX_STABLE            1
+#define CH_EX_STABLE            0
 
 /**
  * @name    ChibiOS/EX version identification
